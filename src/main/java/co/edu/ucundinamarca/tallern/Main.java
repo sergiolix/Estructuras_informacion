52d8348ac74b355ea59564bc25e453de2678d32e
sergiolix <sergiolix8@gmail.com>
2021-08-27 14:08:44 -0500

    Subiendo Esqueleto

---
+++ src/main/java/co/edu/ucundinamarca/tallern/Main.java
@@ -0,0 +1,19 @@
+
+package co.edu.ucundinamarca.tallern;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Clase Principal
+ * @author handresortiz
+ */
+public class Main {
+
+    static Logger log = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        log.debug("Hello World : this is a debug message");
+        log.info("Hello World : this is an info message");
+    }
+
+}
---
+++ src/test/java/co/edu/ucundinamarca/tallern/MainTest.java
@@ -0,0 +1,27 @@
+
+package co.edu.ucundinamarca.tallern;
+
+
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import org.junit.Test;
+
+/**
+ *
+ * @author hortizr
+ */
+public class MainTest {
+
+    @Test
+    public void testSuma() {
+        String text = "Text";
+        assertEquals("Text", text);
+    }
+
+    @Test
+    public void testSuma2() {
+        assertTrue(1 + 1 == 2);
+    }
+
+}
